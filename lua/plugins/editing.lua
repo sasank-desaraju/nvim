a3bcb04cf70e141b0b4f4da045185cc7e2d2440a
sasank-desaraju <sasank.desaraju@ufl.edu>
2026-03-09 19:37:14 -0400

    codex did some more work lol

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -181,9 +181,9 @@ return {
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     build = "cd app && npm install",
     init = function()
-      vim.g.mkdp_filetypes = { "markdown" }
+      vim.g.mkdp_filetypes = { "markdown", "quarto" }
     end,
-    ft = { "markdown" },
+    ft = { "markdown", "quarto" },
     keys = {
       { "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "markdown [p]review" },
     },
